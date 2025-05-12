d31d326ad03cf46cbd1049c4524b156f4eefea6e
Xanooo <tamineha@yahoo.fr>
2025-05-12 10:10:50 +0200

    On créé la galerie et on ajoute d'autres illustrations.

--- view/GalleryView.php
+++ view/GalleryView.php
@@ -15,11 +15,38 @@ class GalleryView
          <h1>Galerie</h1>
          <hr>
          <h3>Retrouvez ici l\'historique des mes dessins.</h3>
-         <img src="#" alt="Dessins réalisés en 2025" />
-         <img src="#" alt="Dessins réalisés en 2024" />
-         <img src="#" alt="Dessins réalisés en 2023" />
-         <img src="#" alt="Dessins réalisés en 2022" />
-
+            <div class="masonry-container">
+                <div class="masonry-grid">
+                    <div class="masonry-item">
+                        <img src="/view/img/Chihiro.png" alt="Dessin de Chihiro, réalisé en 2025" />
+                        <div class="masonry-caption">Chihiro, Le voyage de Chihiro</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/James.png" alt="Dessin de James Lee, réalisé en 2025" />
+                        <div class="masonry-caption">James Lee, Lookism</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/Azurill.png" alt="Dessin d\'Azurill, dans le cadre du projet Pokémon Gen X" />
+                        <div class="masonry-caption">Azurill, Projet Pokémon Gen X</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/Sora.png" alt="Dessin de Tokino Sora, réalisé en 2025" />
+                        <div class="masonry-caption">Tokino Sora, Hololive</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/Mio.png" alt="Dessin de Ookami Mio, réalisé en 2025" />
+                        <div class="masonry-caption">Ookami Mio, Hololive</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/Togetic.png" alt="Dessin de Togetic, réalisé en 2025" />
+                        <div class="masonry-caption">Togetic, Pokémon</div>
+                    </div>
+                    <div class="masonry-item">
+                        <img src="/view/img/Gura.png" alt="Dessin de Gawr Gura, réalisé en 2025" />
+                        <div class="masonry-caption">Gawr Gura, Hololive</div>
+                    </div>
+                </div>
+            </div>
         ';
 
         $this->layout->render($content);
