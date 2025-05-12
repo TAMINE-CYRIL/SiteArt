8b02eaf5df95c8df0c822da44f4dc4ac7d3102d9
Xanooo <tamineha@yahoo.fr>
2025-05-12 11:56:27 +0200

    On change la dimension de nos images.

--- view/GalleryView.php
+++ view/GalleryView.php
@@ -17,32 +17,47 @@ class GalleryView
          <h3>Retrouvez ici l\'historique des mes dessins.</h3>
             <div class="masonry-gallery" id="masonry-gallery">
                 <div class="masonry-item">
-                    <img src="/view/img/Chihiro.png" alt="Dessin de Chihiro, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Chihiro.png" alt="Dessin de Chihiro, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">Chihiro, Le voyage de Chihiro</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/James.png" alt="Dessin de James Lee, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/James.png" alt="Dessin de James Lee, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">James Lee, Lookism</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/Azurill.png" alt="Dessin d\'Azurill, dans le cadre du projet Pokémon Gen X" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Azurill.png" alt="Dessin d\'Azurill, dans le cadre du projet Pokémon Gen X" />
+                    </div>
                     <div class="masonry-caption">Azurill, Projet Pokémon Gen X</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/Sora.png" alt="Dessin de Tokino Sora, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Sora.png" alt="Dessin de Tokino Sora, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">Tokino Sora, Hololive</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/Mio.png" alt="Dessin de Ookami Mio, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Mio.png" alt="Dessin de Ookami Mio, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">Ookami Mio, Hololive</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/Togetic.png" alt="Dessin de Togetic, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Togetic.png" alt="Dessin de Togetic, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">Togetic, Pokémon</div>
                 </div>
                 <div class="masonry-item">
-                    <img src="/view/img/Gura.png" alt="Dessin de Gawr Gura, réalisé en 2025" />
+                    <div class="zoom-container">
+                        <img src="/view/img/Gura.png" alt="Dessin de Gawr Gura, réalisé en 2025" />
+                    </div>
                     <div class="masonry-caption">Gawr Gura, Hololive</div>
+                    <span class="masonry-badge">Nouveau</span>
                 </div>
             </div>
         ';
